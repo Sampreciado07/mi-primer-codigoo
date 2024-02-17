b2ed3daaca93eceaa79a5825b9d95a810f49bea8
Sampreciado07 <160347250+Sampreciado07@users.noreply.github.com>
2024-02-17 14:36:38 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(300, 500);
+  
+}
+
+function draw() {
+  background('#cdf4ff');
+  square(107, 55, 75)
+  circle(170, 90, 15)
+  circle(170, 90, 10)
+  circle(120, 90, 15)
+  circle(120, 90, 10)
+  square(70, 130, 75)
+  square(145, 130, 75)
+  square(97, 205, 100)
+  triangle(97, 305, 145, 350, 197, 305);
+}
